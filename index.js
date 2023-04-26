4dea97362a1ec1f3558ca871fe6ca4cff1e95cc3
mohit <rajpootmohit782@gmail.com>
2023-04-26 07:38:14 +0000

    create server index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log('Request received');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write(`My name is (Mohit Rajpoot)`);
+  res.end();
+});
+
+server.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
